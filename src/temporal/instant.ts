862db95bf81460f695c6f1c98e3fc7dde9fb5aaf
github akito-10 <akt.fukuda@gmail.com>
2023-12-14 23:11:09 +0900

    おいよ

---
+++ src/temporal/instant.ts
@@ -0,0 +1,6 @@
+import { Temporal } from "temporal-polyfill";
+
+const instant = Temporal.Instant.from("2023-12-12T00:00:00Z");
+console.log(instant.toString());
+// output: 2023-12-12T00:00:00Z
+// exact time
