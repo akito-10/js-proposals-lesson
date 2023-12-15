94048b31475066c52d5c418af0ba6888edf1f255
github akito-10 <akt.fukuda@gmail.com>
2023-12-16 03:20:46 +0900

    oiyo

--- src/temporal/instant.ts
+++ src/temporal/instant.ts
@@ -4,3 +4,6 @@ const instant = Temporal.Instant.from("2023-12-12T00:00:00Z");
 console.log(instant.toString());
 // output: 2023-12-12T00:00:00Z
 // exact time
+
+console.log(instant.epochMilliseconds);
+// output: 1702339200000
